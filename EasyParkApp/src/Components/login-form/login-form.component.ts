8d5b7e0bde534cdf3e402de26c47087187159104
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-10-17 11:32:28 -0300

    feat: add environment

--- EasyParkApp/src/Components/login-form/login-form.component.ts
+++ EasyParkApp/src/Components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+    loginForm: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+      this.loginForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+    }
+
+    onSubmit() {
+      if (this.loginForm.valid) {
+        console.log('Form Submitted', this.loginForm.value);
+      }
+  }
 }
