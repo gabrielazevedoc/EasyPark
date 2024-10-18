fab09d5c099fb8c120e24d10da565fc626fdf2b7
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-10-18 11:51:20 -0300

    feat: add cadastro form component

---
+++ EasyParkApp/src/app/Components/cadastro-form/cadastro-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-form.component.html',
+  styleUrl: './cadastro-form.component.css'
+})
+export class CadastroFormComponent {
+
+}
