1d0dc3bbd4ebceae320e32a27bc56436a0497cde
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-10-21 12:40:47 -0300

    feat: img config

--- EasyParkAPI/EasyParkAPI/Program.cs
+++ EasyParkAPI/EasyParkAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using EasyParkAPI.Infrastructure;
 using EasyParkAPI.Services.Carro;
 using EasyParkAPI.Services.Reserva;
@@ -9,7 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- EasyParkApp/src/app/app.routes.ts
+++ EasyParkApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { CadastroPageComponent } from './Pages/cadastro-page/cadastro-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Rota padrão
+    { path: 'login', component: LoginPageComponent }, // Rota para HomeComponent
+    { path: 'cadastro', component: CadastroPageComponent }, // Rota para AboutComponent
+];
