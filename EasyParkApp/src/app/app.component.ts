a85080a9ac6da0d9e1037cdd0c23ee68ed135206
Gabriel <gabrielazevedoc15@hotmail.com>
2025-04-21 07:57:13 -0300

    feat: novo componente criado. O campo dashboard

--- EasyParkApp/src/app/app.component.ts
+++ EasyParkApp/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
 import { CadastroPageComponent } from './Pages/cadastro-page/cadastro-page.component';
 import { MenuComponent } from './Components/menu/menu.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, CadastroPageComponent, MenuComponent],
+  imports: [RouterOutlet, LoginPageComponent, CadastroPageComponent, MenuComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
